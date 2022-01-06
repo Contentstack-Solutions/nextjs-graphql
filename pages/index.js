2918f511cbd2ec4885ef70b32870fca10a491887
Muhammed Constantino <44826802+msconstantino@users.noreply.github.com>
2022-01-06 10:15:07 -0800

    init

--- pages/index.js
+++ pages/index.js
@@ -1,39 +1,56 @@
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
 import { gql } from "@apollo/client";
+import { useState, useEffect } from "react";
 import client from "../apollo-client";
 
-export async function getServerSideProps() {
-  const { data } = await client.query({
-    query: gql`
-    query {
-      all_home {
-        items {
-          body
-          header
-          number
-        }
-      }
-    }
-    `,
-  });
+export default function Home() {
+  const [data, setData] = useState({});
+  const [isLoading, setLoading] = useState(true);
 
-  return {
-    props: {
-      data,
-    },
-  };
-}
+  useEffect(() => {
+    const Fetchdata = () => {
+      client.query({
+        query: gql`
+          query {
+            all_home {
+              items {
+                body
+                header
+                number
+              }
+            }
+          }
+        `,
+      })
+        .then((data) => {
+          setData(data);
+          setLoading(false);
+        });
+    };
+    Fetchdata();
+  }, []);
 
-export default function Home({ data }) {
-  const home = data.all_home.items[0]
-  console.log(data)
+  if (isLoading) {
+    return <div className="App">Loading...</div>;
+  }
+
+  if (!data) {
+    return <div className="App">Loading...</div>;
+  }
+
+  const home = data.data.all_home.items[0]
+
   return (
     <div>
       <h1>{home.header}</h1>
       <p>{home.body}</p>
-      <input type="number" id="quantity" name="quantity" min="1" max="50" value={home.number}/>
+      <input
+        type="number"
+        id="quantity"
+        name="quantity"
+        min="1"
+        max="10000"
+        value={home.number}
+      />
     </div>
   );
 }
